a304b882c01526db2e1afb1054fc441cd1cb8cd8
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-23 21:27:08 +0330

    add AppModule

--- app/src/main/java/ir/hajkarami/paging3cachingcompose/data/local/BeerDao.kt
+++ app/src/main/java/ir/hajkarami/paging3cachingcompose/data/local/BeerDao.kt
@@ -8,7 +8,7 @@ import androidx.room.Upsert
 @Dao
 interface BeerDao {
     @Upsert
-    suspend fun upsertAll(beers: List<Unit>)
+    suspend fun upsertAll(beers: List<BeerEntity>)
 
     @Query("SELECT * FROM beerentity")
     fun pagingSource(): PagingSource<Int, BeerEntity>
